7d4acf37a1c49fba718729f52147aba03ae3e00c
Florian Wolters <wolters.fl@gmail.com>
2013-07-29 12:30:23 +0200

    Added automated integration tests for the class FlorianWolters.Reflection.AssemblyInfo.

---
+++ FlorianWolters.Reflection.Tests/AssemblyInfoIntegrationTest.cs
@@ -0,0 +1,95 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="AssemblyInfoIntegrationTest.cs" company="Florian Wolters">
+//     Copyright (c) Florian Wolters. All rights reserved.
+// </copyright>
+// <author>Florian Wolters &lt;wolters.fl@gmail.com&gt;</author>
+//------------------------------------------------------------------------------
+namespace FlorianWolters.Reflection
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AssemblyInfoIntegrationTest
+    {
+        private AssemblyInfo assemblyInfo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.assemblyInfo = new AssemblyInfo(Assembly.GetExecutingAssembly());
+        }
+
+        [Test]
+        public void TestConstructorThrowsArgumentNullExceptionIfAssemblyIsNull()
+        {
+            Assert.That(
+                () => new AssemblyInfo(null),
+                Throws.Exception.TypeOf<ArgumentNullException>().With.Property("ParamName").EqualTo("assembly"));
+        }
+
+        [Test]
+        public void TestCompany()
+        {
+            string expected = "Florian Wolters";
+            string actual = this.assemblyInfo.Company;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCopyright()
+        {
+            string expected = "Copyright © Florian Wolters 2013";
+            string actual = this.assemblyInfo.Copyright;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestDescription()
+        {
+            string expected = "Automatic tests for the namespace FlorianWolters.Reflection";
+            string actual = this.assemblyInfo.Description;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestProduct()
+        {
+            string expected = "FlorianWolters.Reflection.Tests";
+            string actual = this.assemblyInfo.Product;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestTitle()
+        {
+            string expected = "FlorianWolters.Reflection.Tests";
+            string actual = this.assemblyInfo.Title;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestVersion()
+        {
+            Version expected = new Version("0.1.0.0");
+            Version actual = this.assemblyInfo.Version;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCodeBasePath()
+        {
+            string actual = this.assemblyInfo.CodeBasePath;
+
+            Assert.True(Directory.Exists(actual));
+        }
+    }
+}
---
+++ FlorianWolters.Reflection.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über folgende 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("FlorianWolters.Reflection.Tests")]
+[assembly: AssemblyDescription("Automatic tests for the namespace FlorianWolters.Reflection")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Florian Wolters")]
+[assembly: AssemblyProduct("FlorianWolters.Reflection.Tests")]
+[assembly: AssemblyCopyright("Copyright © Florian Wolters 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Wenn ComVisible auf "false" festgelegt wird, sind die Typen innerhalb dieser Assembly 
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("3815529c-ddb3-48e4-96fc-f187807bd357")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// durch Einsatz von '*', wie in nachfolgendem Beispiel:
+// [Assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
