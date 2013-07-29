4e42112f3e996cf2468eb494444a553c9d014fd7
Florian Wolters <wolters.fl@gmail.com>
2013-07-29 19:07:38 +0200

    Made source code fulfill StyleCop settings.

--- FlorianWolters.Reflection.Tests/Properties/AssemblyInfo.cs
+++ FlorianWolters.Reflection.Tests/Properties/AssemblyInfo.cs
@@ -1,10 +1,13 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿//------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Florian Wolters">
+//     Copyright (c) Florian Wolters. All rights reserved.
+// </copyright>
+// <author>Florian Wolters &lt;wolters.fl@gmail.com&gt;</author>
+//------------------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über folgende 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("FlorianWolters.Reflection.Tests")]
 [assembly: AssemblyDescription("Automatic tests for the namespace FlorianWolters.Reflection")]
 [assembly: AssemblyConfiguration("")]
@@ -13,24 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Florian Wolters 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Wenn ComVisible auf "false" festgelegt wird, sind die Typen innerhalb dieser Assembly 
-// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von 
-// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
 [assembly: ComVisible(false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("3815529c-ddb3-48e4-96fc-f187807bd357")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// durch Einsatz von '*', wie in nachfolgendem Beispiel:
-// [Assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
