a70a73af1301fecb191b5576c3882b6cd820dcb1
Florian Wolters <wolters.fl@gmail.com>
2013-07-29 13:18:04 +0200

    Added dialog to compare two Word documents.

---
+++ FlorianWolters.Office.Word/Dialogs/CompareDocumentsDialog.cs
@@ -0,0 +1,25 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="CompareDocumentsDialog.cs" company="Florian Wolters">
+//     Copyright (c) Florian Wolters. All rights reserved.
+// </copyright>
+// <author>Florian Wolters &lt;wolters.fl@gmail.com&gt;</author>
+//------------------------------------------------------------------------------
+
+namespace FlorianWolters.Office.Word.Dialogs
+{
+    using Word = Microsoft.Office.Interop.Word;
+
+    public class CompareDocumentsDialog : Dialog
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see
+        /// cref="CompareDocumentsDialog"/> class with the specified <see
+        /// cref="Word.Application"/>.
+        /// </summary>
+        /// <param name="application">The Microsoft Word application.</param>
+        public CompareDocumentsDialog(Word.Application application)
+            : base(application, Word.WdWordDialog.wdDialogToolsCompareDocuments)
+        {
+        }
+    }
+}
