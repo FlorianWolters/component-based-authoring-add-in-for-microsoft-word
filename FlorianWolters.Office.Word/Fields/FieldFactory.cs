d67970ae73957262564b8ba469bb49223f0bbabe
Florian Wolters <wolters.fl@gmail.com>
2013-08-05 13:08:02 +0200

    Added "\d" switch for INCLUDEPICTURE field and formatted source code according to StyleCop.

--- FlorianWolters.Office.Word/Fields/FieldFactory.cs
+++ FlorianWolters.Office.Word/Fields/FieldFactory.cs
@@ -14,16 +14,46 @@ namespace FlorianWolters.Office.Word.Fields
     using FlorianWolters.Office.Word.DocumentProperties;
     using Word = Microsoft.Office.Interop.Word;
 
+    /// <summary>
+    /// The class <see cref="FieldFactory"/> simplifies the creation of new <see
+    /// cref="Word.Field"/>s.
+    /// </summary>
     public class FieldFactory
     {
+        /// <summary>
+        /// The <see cref="Word.Application"/> to interact with.
+        /// </summary>
         private readonly Word.Application application;
+
+        /// <summary>
+        /// The <see cref="CustomDocumentPropertyReader"/> used to read custom
+        /// document properties from the <see cref="Word.Document"/> of the <see
+        /// cref="Word.Application"/> to interact with.
+        /// </summary>
         private readonly CustomDocumentPropertyReader customDocumentPropertyReader;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FieldFactory"/> class.
+        /// </summary>
+        /// <param name="application">
+        /// The <see cref="Word.Application"/> to interact with.
+        /// </param>
         public FieldFactory(Word.Application application)
             : this(application, new CustomDocumentPropertyReader())
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FieldFactory"/> class.
+        /// </summary>
+        /// <param name="application">
+        /// The <see cref="Word.Application"/> to interact with.
+        /// </param>
+        /// <param name="customDocumentPropertyReader">
+        /// The <see cref="CustomDocumentPropertyReader"/> used to read custom
+        /// document properties from the <see cref="Word.Document"/> of the <see
+        /// cref="Word.Application"/> to interact with.
+        /// </param>
         public FieldFactory(
             Word.Application application,
             CustomDocumentPropertyReader customDocumentPropertyReader)
@@ -32,6 +62,15 @@ namespace FlorianWolters.Office.Word.Fields
             this.customDocumentPropertyReader = customDocumentPropertyReader;
         }
 
+        /// <summary>
+        /// Inserts a new <i>DATE</i> <see cref="Word.Field"/> into the current
+        /// <see cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="mergeFormat">
+        /// Whether to apply the formatting of the previous field result to the
+        /// new result.
+        /// </param>
+        /// <returns>The newly created <see cref="Word.Field"/>.</returns>
         public Word.Field InsertDate(bool mergeFormat = false)
         {
             return this.AddFieldToCurrentSelection(
@@ -39,9 +78,24 @@ namespace FlorianWolters.Office.Word.Fields
                 mergeFormat);
         }
 
-        public Word.Field InsertDocProperty(string propertyName, bool mergeFormat = false)
+        /// <summary>
+        /// Inserts a new <i>DOCPROPERTY</i> <see cref="Word.Field"/> into the
+        /// current <see cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="propertyName">
+        /// The name of a custom document property.
+        /// </param>
+        /// <param name="mergeFormat">
+        /// Whether to apply the formatting of the previous field result to the
+        /// new result.
+        /// </param>
+        /// <returns>The newly created <see cref="Word.Field"/>.</returns>
+        public Word.Field InsertDocProperty(
+            string propertyName,
+            bool mergeFormat = false)
         {
-            this.customDocumentPropertyReader.Load(this.application.ActiveDocument);
+            this.customDocumentPropertyReader.Load(
+                this.application.ActiveDocument);
             this.customDocumentPropertyReader.Get(propertyName);
 
             return this.AddFieldToCurrentSelection(
@@ -50,7 +104,19 @@ namespace FlorianWolters.Office.Word.Fields
                 propertyName);
         }
 
-        public Word.Field InsertEmpty(string data = null, bool mergeFormat = false)
+        /// <summary>
+        /// Inserts a new empty <see cref="Word.Field"/> into the current <see
+        /// cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="data">The optional data of the field.</param>
+        /// <param name="mergeFormat">
+        /// Whether to apply the formatting of the previous field result to the
+        /// new result.
+        /// </param>
+        /// <returns>The newly created <see cref="Word.Field"/>.</returns>
+        public Word.Field InsertEmpty(
+            string data = null,
+            bool mergeFormat = false)
         {
             return this.AddFieldToCurrentSelection(
                 Word.WdFieldType.wdFieldEmpty,
@@ -58,6 +124,15 @@ namespace FlorianWolters.Office.Word.Fields
                 data);
         }
 
+        /// <summary>
+        /// Inserts a new <i>LISTNUM</i> <see cref="Word.Field"/> into the
+        /// current <see cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="mergeFormat">
+        /// Whether to apply the formatting of the previous field result to the
+        /// new result.
+        /// </param>
+        /// <returns>The newly created <see cref="Word.Field"/>.</returns>
         public Word.Field InsertListNum(bool mergeFormat = false)
         {
             return this.AddFieldToCurrentSelection(
@@ -65,6 +140,15 @@ namespace FlorianWolters.Office.Word.Fields
                 mergeFormat);
         }
 
+        /// <summary>
+        /// Inserts a new <i>PAGE</i> <see cref="Word.Field"/> into the current
+        /// <see cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="mergeFormat">
+        /// Whether to apply the formatting of the previous field result to the
+        /// new result.
+        /// </param>
+        /// <returns>The newly created <see cref="Word.Field"/>.</returns>
         public Word.Field InsertPage(bool mergeFormat = false)
         {
             return this.AddFieldToCurrentSelection(
@@ -72,6 +156,15 @@ namespace FlorianWolters.Office.Word.Fields
                 mergeFormat);
         }
 
+        /// <summary>
+        /// Inserts a new <i>TIME</i> <see cref="Word.Field"/> into the current
+        /// <see cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="mergeFormat">
+        /// Whether to apply the formatting of the previous field result to the
+        /// new result.
+        /// </param>
+        /// <returns>The newly created <see cref="Word.Field"/>.</returns>
         public Word.Field InsertTime(bool mergeFormat = false)
         {
             return this.AddFieldToCurrentSelection(
@@ -79,7 +172,19 @@ namespace FlorianWolters.Office.Word.Fields
                 mergeFormat);
         }
 
-        public Word.Field InsertIncludePicture(string filePath, bool mergeFormat = false)
+        /// <summary>
+        /// Inserts a new <i>INCLUDEPICTURE</i> <see cref="Word.Field"/> into
+        /// the current <see cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="filePath">The file path of the file to include.</param>
+        /// <param name="mergeFormat">
+        /// Whether to apply the formatting of the previous field result to the
+        /// new result.
+        /// </param>
+        /// <returns>The newly created <see cref="Word.Field"/>.</returns>
+        public Word.Field InsertIncludePicture(
+            string filePath,
+            bool mergeFormat = false)
         {
             this.ThrowFileNotFoundExceptionIfFileDoesNotExist(filePath);
 
@@ -89,7 +194,19 @@ namespace FlorianWolters.Office.Word.Fields
                 filePath);
         }
 
-        public Word.Field InsertIncludeText(string filePath, bool mergeFormat = false)
+        /// <summary>
+        /// Inserts a new <i>INCLUDETEXT</i> <see cref="Word.Field"/> into the
+        /// current <see cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="filePath">The file path of the file to include.</param>
+        /// <param name="mergeFormat">
+        /// Whether to apply the formatting of the previous field result to the
+        /// new result.
+        /// </param>
+        /// <returns>The newly created <see cref="Word.Field"/>.</returns>
+        public Word.Field InsertIncludeText(
+            string filePath,
+            bool mergeFormat = false)
         {
             this.ThrowFileNotFoundExceptionIfFileDoesNotExist(filePath);
 
@@ -99,16 +216,61 @@ namespace FlorianWolters.Office.Word.Fields
                 filePath);
         }
 
-        public void InsertIncludePictureWithNestedDocProperty(string filePath, string propertyName)
+        /// <summary>
+        /// Inserts a new <i>INCLUDEPICTURE</i> <see cref="Word.Field"/> which
+        /// contains a nested <i>DOCPROPERTY</i> <see cref="Word.Field"/> into
+        /// the current <see cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="filePath">The file path of the file to include.</param>
+        /// <param name="propertyName">
+        /// The name of the nested <i>DOCPROPERTY</i> field.
+        /// </param>
+        public void InsertIncludePictureWithNestedDocProperty(
+            string filePath,
+            string propertyName)
         {
-            this.InsertIncludeFieldWithNestedDocProperty(filePath, propertyName, "INCLUDEPICTURE");
+            this.InsertIncludeFieldWithNestedDocProperty(
+                filePath,
+                propertyName,
+                "INCLUDEPICTURE");
+            
+            // Append the "\d" switch to avoid that the graphics data is saved
+            // in the Word document.
+            this.application.Selection.Range.InsertAfter("\\d");
         }
 
-        public void InsertIncludeTextWithNestedDocProperty(string filePath, string propertyName)
+        /// <summary>
+        /// Inserts a new <i>INCLUDETEST</i> <see cref="Word.Field"/> which
+        /// contains a nested <i>DOCPROPERTY</i> <see cref="Word.Field"/> into
+        /// the current <see cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="filePath">The file path of the file to include.</param>
+        /// <param name="propertyName">
+        /// The name of the nested <i>DOCPROPERTY</i> field.
+        /// </param>
+        public void InsertIncludeTextWithNestedDocProperty(
+            string filePath,
+            string propertyName)
         {
-            this.InsertIncludeFieldWithNestedDocProperty(filePath, propertyName, "INCLUDETEXT");
+            this.InsertIncludeFieldWithNestedDocProperty(
+                filePath,
+                propertyName,
+                "INCLUDETEXT");
         }
 
+        /// <summary>
+        /// Adds the specified type of <see cref="Word.Field"/> to the current
+        /// <see cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="type">
+        /// The type of <see cref="Word.Field"/> to create.
+        /// </param>
+        /// <param name="mergeFormat">
+        /// Whether to apply the formatting of the previous field result to the
+        /// new result.
+        /// </param>
+        /// <param name="text">The text of the <see cref="Word.Field"/>.</param>
+        /// <returns>The newly created <see cref="Word.Field"/>.</returns>
         private Word.Field AddFieldToCurrentSelection(
             Word.WdFieldType type,
             bool mergeFormat = false,
@@ -135,6 +297,14 @@ namespace FlorianWolters.Office.Word.Fields
             return result;
         }
 
+        /// <summary>
+        /// Throws a new <see cref="FileNotFoundException"/> if the specified
+        /// file path does not exist.
+        /// </summary>
+        /// <param name="filePath">The file path to check.</param>
+        /// <exception cref="FileNotFoundException">
+        /// If the specified file path does not exist.
+        /// </exception>
         private void ThrowFileNotFoundExceptionIfFileDoesNotExist(string filePath)
         {
             if (!File.Exists(filePath))
@@ -146,6 +316,19 @@ namespace FlorianWolters.Office.Word.Fields
 
         // TODO Find a better solution.
         // http://stackoverflow.com/questions/16774411/create-a-nested-field-with-visual-studio-tools-for-office-vsto
+       
+        /// <summary>
+        /// Inserts a new <i>INCLUDE[...]</i> <see cref="Word.Field"/> into
+        /// the current <see cref="Word.Selection"/>.
+        /// </summary>
+        /// <param name="filePath">The file path of the file to include.</param>
+        /// <param name="propertyName">
+        /// The name of the nested <i>DOCPROPERTY</i> field.
+        /// </param>
+        /// <param name="functionName">
+        /// The name of the <see cref="Word.Field"/> function, e.g.
+        /// <c>INCLUDETEXT</c>.
+        /// </param>
         private void InsertIncludeFieldWithNestedDocProperty(
             string filePath,
             string propertyName,
