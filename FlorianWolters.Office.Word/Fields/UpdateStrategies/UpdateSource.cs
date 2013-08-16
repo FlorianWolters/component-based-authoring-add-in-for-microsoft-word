5ad72f98b6454a89e0636630870ebdc67bdc20dc
Florian Wolters <wolters.fl@gmail.com>
2013-08-16 14:23:31 +0200

    Added XMLDoc and reorganized conditions.

--- FlorianWolters.Office.Word/Fields/UpdateStrategies/UpdateSource.cs
+++ FlorianWolters.Office.Word/Fields/UpdateStrategies/UpdateSource.cs
@@ -12,8 +12,15 @@ namespace FlorianWolters.Office.Word.Fields.UpdateStrategies
     using FlorianWolters.Office.Word.Extensions;
     using Word = Microsoft.Office.Interop.Word;
 
+    /// <summary>
+    /// The class <see cref="UpdateSource"/> saves the changes made to the results of an INCLUDETEXT (or INCLUDE) <see
+    /// cref="Word.Field"/> back to the source document.
+    /// </summary>
     public class UpdateSource : IUpdateStrategy
     {
+        /// <summary>
+        /// The error code which determines that the source document is read-only.
+        /// </summary>
         private const int ErrorCodeReadOnly = -2146823133;
 
         /// <summary>
@@ -22,24 +29,21 @@ namespace FlorianWolters.Office.Word.Fields.UpdateStrategies
         /// <param name="field">The <see cref="Word.Field"/> to update.</param>
         public void Update(Word.Field field)
         {
-            if (field.CanUpdateSource())
-            {
-                try
-                {
-                    field.UpdateSource();
-                }
-                catch (COMException ex)
-                {
-                    if (ErrorCodeReadOnly == ex.ErrorCode)
-                    {
-                        throw new ReadOnlyDocumentException(
-                            "The included document is read-only.");
-                    }
-                }
-            }
-            else
+            if (!field.CanUpdateSource())
             {
                 throw new ArgumentException("Invalid field type.");
+            }
+
+            try
+            {
+                field.UpdateSource();
+            }
+            catch (COMException ex)
+            {
+                if (ErrorCodeReadOnly == ex.ErrorCode)
+                {
+                    throw new ReadOnlyDocumentException("The included document is read-only.");
+                }
             }
         }
     }
