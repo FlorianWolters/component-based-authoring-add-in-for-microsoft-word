c76f7283dedb4c6df44b007cfb3a8dffe458ee06
Florian Wolters <wolters.fl@gmail.com>
2013-08-16 15:28:30 +0200

    Fixed method signature.

--- FlorianWolters.Office.Word/Event/EventHandlers/IWindowBeforeDoubleClickEventHandler.cs
+++ FlorianWolters.Office.Word/Event/EventHandlers/IWindowBeforeDoubleClickEventHandler.cs
@@ -9,8 +9,22 @@ namespace FlorianWolters.Office.Word.Event.EventHandlers
 {
     using Word = Microsoft.Office.Interop.Word;
 
+    /// <summary>
+    /// The interface <see cref="IWindowBeforeDoubleClickEventHandler"/> allows to handle the event which occurs when 
+    /// the editing area of a <see cref="Word.Document"/> <see cref="Word.Window"/> is double-clicked, before the
+    /// default double-click action.
+    /// </summary>
     public interface IWindowBeforeDoubleClickEventHandler : IEventHandler
     {
-        void OnWindowBeforeRightClick(Word.Window window);
+        /// <summary>
+        /// Handles the event which occurs when the editing area of a <see cref="Word.Document"/> <see
+        /// cref="Word.Window"/> is double-clicked, before the default double-click action.
+        /// </summary>
+        /// <param name="selection">The current <see cref="Word.Selection"/>.</param>
+        /// <param name="cancel">
+        /// <c>false</c> when the event occurs. If the event procedure sets this argument to <c>true</c>, the default
+        /// double-click action does not occur when the procedure is finished.
+        /// </param>
+        void OnWindowBeforeDoubleClick(Word.Selection selection, ref bool cancel);
     }
 }
--- FlorianWolters.Office.Word/Event/EventHandlers/IWindowBeforeRightClickEventHandler.cs
+++ FlorianWolters.Office.Word/Event/EventHandlers/IWindowBeforeRightClickEventHandler.cs
@@ -9,8 +9,21 @@ namespace FlorianWolters.Office.Word.Event.EventHandlers
 {
     using Word = Microsoft.Office.Interop.Word;
 
+    /// <summary>
+    /// The interface <see cref="IWindowBeforeRightClickEventHandler"/> allows to handle the event which occurs when the
+    /// editing area of a document window is right-clicked, before the default right-click action.
+    /// </summary>
     public interface IWindowBeforeRightClickEventHandler : IEventHandler
     {
-        void OnWindowBeforeRightClick(Word.Windows window);
+        /// <summary>
+        /// Handles the event which occurs when the editing area of a document window is right-clicked, before the
+        /// default right-click action.
+        /// </summary>
+        /// <param name="selection">The current <see cref="Word.Selection"/>.</param>
+        /// <param name="cancel">
+        /// <c>false</c> when the event occurs. If the event procedure sets this argument to <c>true</c>, the default
+        /// right-click action does not occur when the procedure is finished.
+        /// </param>
+        void OnWindowBeforeRightClick(Word.Selection selection, ref bool cancel);
     }
 }
