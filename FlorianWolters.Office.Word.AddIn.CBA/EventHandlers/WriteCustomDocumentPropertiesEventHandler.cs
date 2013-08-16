28efd24e45bf49da36da2dd40aacc18e4b2f494c
Florian Wolters <wolters.fl@gmail.com>
2013-08-16 15:28:50 +0200

    Added exception handling.

--- FlorianWolters.Office.Word.AddIn.CBA/EventHandlers/WriteCustomDocumentPropertiesEventHandler.cs
+++ FlorianWolters.Office.Word.AddIn.CBA/EventHandlers/WriteCustomDocumentPropertiesEventHandler.cs
@@ -7,6 +7,7 @@
 
 namespace FlorianWolters.Office.Word.AddIn.CBA.EventHandlers
 {
+    using System;
     using FlorianWolters.Office.Word.Commands;
     using FlorianWolters.Office.Word.Event.EventHandlers;
     using FlorianWolters.Office.Word.Event.ExceptionHandlers;
@@ -32,32 +33,47 @@ namespace FlorianWolters.Office.Word.AddIn.CBA.EventHandlers
         /// <summary>
         /// Handles the event which occurs when a <see cref="Word.Document"/> is opened.
         /// </summary>
-        /// <param name="document">The <see cref="Word.Document"/>.</param>
+        /// <param name="document">The <see cref="Word.Document"/> that's being opened.</param>
         public void OnDocumentOpen(Word.Document document)
         {
             this.TryExecute();
         }
 
         /// <summary>
-        /// Handles the event which occurs before a <see cref="Word.Document"/> is saved.
+        /// Handles the event which occurs before any <see cref="Word.Document"/> is saved.
         /// </summary>
         /// <param name="document">The <see cref="Word.Document"/> that's being saved.</param>
-        /// <param name="saveAsUI"><c>true</c> if called via <i>Save As</i>; <c>false</c> if called via <i>Save</i>.</param>
-        /// <param name="cancel"><c>true</c> to prevent the <see cref="Word.Document"/> from being saved; <c>false</c> otherwise.</param>
+        /// <param name="saveAsUI">
+        /// <c>true</c> if the <b>Save As</b> dialog box is displayed, whether to save a new <see
+        /// cref="Word.Document"/>, in response to the <b>Save</b> command; or in response to the <b>Save As</b>
+        /// command; or in response to the <b>SaveAs</b> or <b>SaveAs2</b> method.
+        /// </param>
+        /// <param name="cancel">
+        /// <c>false</c> when the event occurs. If the event procedure sets this argument to <c>true</c>, the <see
+        /// cref="Word.Document"/> is not saved when the procedure is finished.
+        /// </param>
         public void OnDocumentBeforeSave(Word.Document document, ref bool saveAsUI, ref bool cancel)
         {
             // Solution taken from the following Microsoft Developer Network (MSDN) thread:
-            // http://social.msdn.microsoft.com/Forums/4db4878e-a27c-4a6c-9c7d-984d918c0db5/how-to-call-some-code-after-document-is-saved-in-word
-
-            // Check whether the document has changed since it was last saved.
-            if (!document.Saved)
+            // http://social.msdn.microsoft.com/Forums/en-US/4db4878e-a27c-4a6c-9c7d-984d918c0db5/how-to-call-some-code-after-document-is-saved-in-word
+            try
             {
-                // Cancel this save operation.
-                cancel = true;
+                // Check whether the document has changed since it was last saved.
+                if (!document.Saved)
+                {
+                    // Cancel this save operation.
+                    cancel = true;
 
-                // Save the document and execute the Command.
-                document.Save();
-                this.TryExecute();
+                    // Save the document and execute the Command.
+                    document.Save();
+                    this.Command.Execute();
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO This solution isn't perfect. If the method Save is called, a possible save failuere is not 
+                // handled by the Microsoft Word application. Therefore this differs from the default behaviour.
+                this.ExceptionHandler.HandleException(ex);
             }
         }
     }
