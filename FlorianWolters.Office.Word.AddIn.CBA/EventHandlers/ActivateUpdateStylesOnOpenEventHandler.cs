9d9f87783b95a62b618ba20d38d31b64f5f09cf2
Florian Wolters <wolters.fl@gmail.com>
2013-08-16 15:29:39 +0200

    Removed implementation of IDocumentBeforeCloseEventHandler.

--- FlorianWolters.Office.Word.AddIn.CBA/EventHandlers/ActivateUpdateStylesOnOpenEventHandler.cs
+++ FlorianWolters.Office.Word.AddIn.CBA/EventHandlers/ActivateUpdateStylesOnOpenEventHandler.cs
@@ -12,34 +12,27 @@ namespace FlorianWolters.Office.Word.AddIn.CBA.EventHandlers
     using FlorianWolters.Office.Word.Event.ExceptionHandlers;
     using Word = Microsoft.Office.Interop.Word;
 
-    internal class ActivateUpdateStylesOnOpenEventHandler
-        : CommandEventHandler,
-        IDocumentBeforeCloseEventHandler,
-        IDocumentOpenEventHandler
+    /// <summary>
+    /// The class <see cref="ActivateUpdateStylesOnOpenEventHandler"/> implements <i>Event Handler</i> methods which
+    /// execute the <see cref="ActivateUpdateStylesOnOpenCommand"/>.
+    /// </summary>
+    internal class ActivateUpdateStylesOnOpenEventHandler : CommandEventHandler, IDocumentOpenEventHandler
     {
-        // TODO Duplicated source code with UpdateAttachedTemplateEventHandler.
-        // But I don't know how to minimize code duplication further without
-        // losing flexibility. It is possible to automatically implement the
-        // "On[...]" methods in dependency of the implemented interfaces?
-
         /// <summary>
-        /// Initializes a new instance of the <see
-        /// cref="ActivateUpdateStylesOnOpenEventHandler"/> class.
+        /// Initializes a new instance of the <see cref="ActivateUpdateStylesOnOpenEventHandler"/> class.
         /// </summary>
         /// <param name="command">The <i>Command</i> to execute with this <i>Event Handler</i>.</param>
         /// <param name="exceptionHandler">The <i>Event Handler</i> used to handle exceptions.</param>
-        public ActivateUpdateStylesOnOpenEventHandler(
-            ICommand command, IExceptionHandler exceptionHandler)
+        public ActivateUpdateStylesOnOpenEventHandler(ICommand command, IExceptionHandler exceptionHandler)
             : base(command, exceptionHandler)
         {
         }
 
+        /// <summary>
+        /// Handles the event which occurs when a <see cref="Word.Document"/> is opened.
+        /// </summary>
+        /// <param name="document">The <see cref="Word.Document"/> that's being opened.</param>
         public void OnDocumentOpen(Word.Document document)
-        {
-            this.TryExecute();
-        }
-
-        public void OnDocumentBeforeClose(Word.Document document, ref bool target)
         {
             this.TryExecute();
         }
--- FlorianWolters.Office.Word.AddIn.CBA/EventHandlers/UpdateAttachedTemplateEventHandler.cs
+++ FlorianWolters.Office.Word.AddIn.CBA/EventHandlers/UpdateAttachedTemplateEventHandler.cs
@@ -12,29 +12,27 @@ namespace FlorianWolters.Office.Word.AddIn.CBA.EventHandlers
     using FlorianWolters.Office.Word.Event.ExceptionHandlers;
     using Word = Microsoft.Office.Interop.Word;
 
-    internal class UpdateAttachedTemplateEventHandler
-        : CommandEventHandler,
-        IDocumentBeforeCloseEventHandler,
-        IDocumentOpenEventHandler
+    /// <summary>
+    /// The class <see cref="UpdateAttachedTemplateEventHandler"/> implements <i>Event Handler</i> methods which execute
+    /// the <see cref="UpdateAttachedTemplateCommand"/>.
+    /// </summary>
+    internal class UpdateAttachedTemplateEventHandler : CommandEventHandler, IDocumentOpenEventHandler
     {
         /// <summary>
-        /// Initializes a new instance of the <see
-        /// cref="UpdateAttachedTemplateEventHandler"/> class.
+        /// Initializes a new instance of the <see cref="UpdateAttachedTemplateEventHandler"/> class.
         /// </summary>
         /// <param name="command">The <i>Command</i> to execute with this <i>Event Handler</i>.</param>
         /// <param name="exceptionHandler">The <i>Event Handler</i> used to handle exceptions.</param>
-        public UpdateAttachedTemplateEventHandler(
-            ICommand command, IExceptionHandler exceptionHandler)
+        public UpdateAttachedTemplateEventHandler(ICommand command, IExceptionHandler exceptionHandler)
             : base(command, exceptionHandler)
         {
         }
 
+        /// <summary>
+        /// Handles the event which occurs when a <see cref="Word.Document"/> is opened.
+        /// </summary>
+        /// <param name="document">The <see cref="Word.Document"/> that's being opened.</param>
         public void OnDocumentOpen(Word.Document document)
-        {
-            this.TryExecute();
-        }
-
-        public void OnDocumentBeforeClose(Word.Document document, ref bool target)
         {
             this.TryExecute();
         }
