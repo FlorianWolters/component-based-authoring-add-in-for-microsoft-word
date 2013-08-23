2419fe0386079f9baf2d356bde61545bfcf5766d
Florian Wolters <wolters.fl@gmail.com>
2013-08-23 17:03:19 +0200

    Updated project settings and added Sandcastle Help File Builder project "Documentation" .

---
+++ FlorianWolters.Office.Word.Tests/Fields/UpdateStrategies/UpdateSourceUnitTest.cs
@@ -0,0 +1,70 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="UpdateSourceUnitTest.cs" company="Florian Wolters">
+//     Copyright (c) Florian Wolters. All rights reserved.
+// </copyright>
+// <author>Florian Wolters &lt;wolters.fl@gmail.com&gt;</author>
+//------------------------------------------------------------------------------
+
+namespace FlorianWolters.Office.Word.Fields.UpdateStrategies
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Moq;
+    using NUnit.Framework;
+    using Word = Microsoft.Office.Interop.Word;
+
+    [TestFixture]
+    public class UpdateSourceUnitTest
+    {
+        [Test]
+        public void TestUpdateThrowsArgumentException()
+        {
+            // Given
+            Mock<Word.Field> mock = new Mock<Word.Field>();
+            UpdateSource updateStrategy = new UpdateSource();
+
+            // Expect
+            mock.Setup(field => field.Type).Returns(Word.WdFieldType.wdFieldEmpty);
+
+            // When and Then
+            Assert.That(
+                () => updateStrategy.Update(mock.Object),
+                Throws.Exception.TypeOf<ArgumentException>().With.Property("ParamName").EqualTo("field"));
+        }
+
+        [Test]
+        public void TestUpdateThrowsReadOnlyDocumentException()
+        {
+            // Given
+            Mock<COMException> comExceptionMock = new Mock<COMException>();
+            Mock<Word.Field> fieldMock = new Mock<Word.Field>();
+            UpdateSource updateStrategy = new UpdateSource();
+
+            // Expect
+            comExceptionMock.Setup(ex => ex.ErrorCode).Returns(-2146823133);
+            fieldMock.Setup(field => field.Type).Returns(Word.WdFieldType.wdFieldIncludeText);
+            fieldMock.Setup(field => field.UpdateSource()).Throws(comExceptionMock.Object);
+
+            // When and Then
+            fieldMock.Verify(field => field.UpdateSource(), Times.Never());
+            Assert.Throws<ReadOnlyDocumentException>(() => updateStrategy.Update(fieldMock.Object));
+        }
+
+        [Test]
+        public void TestUpdate()
+        {
+            // Given
+            Mock<Word.Field> mock = new Mock<Word.Field>();
+            UpdateSource updateStrategy = new UpdateSource();
+
+            // Expect
+            mock.Setup(field => field.Type).Returns(Word.WdFieldType.wdFieldIncludeText);
+
+            // When
+            updateStrategy.Update(mock.Object);
+
+            // Then
+            mock.Verify(field => field.UpdateSource(), Times.Once());
+        }
+    }
+}
---
+++ FlorianWolters.Office.Word.Tests/Fields/UpdateStrategies/UpdateTargetUnitTest.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="UpdateTargetUnitTest.cs" company="Florian Wolters">
+//     Copyright (c) Florian Wolters. All rights reserved.
+// </copyright>
+// <author>Florian Wolters &lt;wolters.fl@gmail.com&gt;</author>
+//------------------------------------------------------------------------------
+
+namespace FlorianWolters.Office.Word.Fields.UpdateStrategies
+{
+    using Moq;
+    using NUnit.Framework;
+    using Word = Microsoft.Office.Interop.Word;
+
+    [TestFixture]
+    public class UpdateTargetUnitTest
+    {
+        [Test]
+        public void TestUpdate()
+        {
+            // Given
+            Mock<Word.Field> mock = new Mock<Word.Field>();
+            UpdateTarget updateTarget = new UpdateTarget();
+
+            // Expect
+            mock.Setup(field => field.Update())
+                .Returns(true);
+
+            // When
+            updateTarget.Update(mock.Object);
+
+            // Then
+            mock.Verify(field => field.Update(), Times.Once());
+        }
+    }
+}
